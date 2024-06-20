d45fd0cf65ebd76fa8f7797695fb563911e5f426
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-20 10:28:10 +0200

    update

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,3 +1,30 @@
+const bcrypt = require('bcrypt');
+
+const Event = require('../../models/event.js');
+const User = require('../../models/user.js');
+
+const events = eventIds => {
+    return Event.find({ _id: { $in: eventIds } })
+        .then(events => {
+            return events.map(event => {
+                return { ...event._doc, _id: event.id, creator: user.bind(this, event.creator) };
+            })
+        })
+        .catch(err => {
+            throw err;
+        });
+};
+
+const user = userId => {
+    return User.findById(userId)
+        .then(user => {
+            return { ...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) };
+        })
+        .catch(err => {
+            throw err;
+        });
+};
+
 module.exports = {
     events: () => {
         return Event.find()
